872a56fdef4124e50992d41a8e4047e3d2dee701
stephincjose <stephinjosec@gmail.com>
2024-04-04 21:57:35 +0100

    creating new html file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-
-from flask import Flask
+from flask import Flask, render_template
 import requests 
+import json
 
 app = Flask(__name__)
 
@@ -12,7 +11,7 @@ def index():
     req = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dublin?unitGroup=metric&include=days%2Chours%2Calerts%2Ccurrent&key=TA8XSWUVK2M3S7GXQHHJ3349L&contentType=json')
     #req = requests.get('https://cat-fact.herokuapp.com/facts')
     print(req.content)
-    return f"1.Running Flask on Google Colab!"
+    return render_template('index.html')
 
 app.run()
 
