8fd5885e8a4107f3328f84fb5621d37cd59222f1
stephincjose <stephinjosec@gmail.com>
2024-04-04 22:20:24 +0100

    convert the json text to python object

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def index():
     req = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dublin?unitGroup=metric&include=days%2Chours%2Calerts%2Ccurrent&key=TA8XSWUVK2M3S7GXQHHJ3349L&contentType=json')
     #req = requests.get('https://cat-fact.herokuapp.com/facts')
     #print(req.content)
-    data = req.content
+    data = json.loads(req.content)
 
     return render_template('index.html',data=data)
 
