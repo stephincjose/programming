7b465639836f822a819834cb140fc02c29344b82
stephincjose <stephinjosec@gmail.com>
2024-04-04 21:52:28 +0100

    adding api

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 
+
 from flask import Flask
+import requests 
 
 app = Flask(__name__)
 
  
  
-@app.route("/")
-def home():
-    return f"Running Flask on Google Colab!"
-
-@app.route("/Second")
-def home():
-    return f"Second Page"
-
-
+@app.route("/", methods=['GET'])
+def index():
+    req = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dublin?unitGroup=metric&include=days%2Chours%2Calerts%2Ccurrent&key=TA8XSWUVK2M3S7GXQHHJ3349L&contentType=json')
+    #req = requests.get('https://cat-fact.herokuapp.com/facts')
+    print(req.content)
+    return f"1.Running Flask on Google Colab!"
 
 app.run()
 
