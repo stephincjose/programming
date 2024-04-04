7e3686ea67e0dc1abe8702c1d97dd6c9d6bcdf9a
stephincjose <stephinjosec@gmail.com>
2024-04-04 22:13:59 +0100

    printing the data collected from api in index.html

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ app = Flask(__name__)
 def index():
     req = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dublin?unitGroup=metric&include=days%2Chours%2Calerts%2Ccurrent&key=TA8XSWUVK2M3S7GXQHHJ3349L&contentType=json')
     #req = requests.get('https://cat-fact.herokuapp.com/facts')
-    print(req.content)
-    return render_template('index.html')
+    #print(req.content)
+    data = req.content
+
+    return render_template('index.html',data=data)
 
 app.run()
 
