5cead94e260855256ccf95a092f4147cd0115643
stephincjose <stephinjosec@gmail.com>
2024-04-05 10:28:14 +0100

    updated the api after changing it accidently

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    req = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dublin?unitGroup=metric&include=days%2Chours%2Calerts%2Ccurrent&key=TA8XSWUVK2M3S7GXQHHJ3349L&contentType=json')
+    req = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dublin?unitGroup=metric&key=TA8XSWUVK2M3S7GXQHHJ3349L&contentType=json')
     #req = requests.get('https://cat-fact.herokuapp.com/facts')
     #print(req.content)
     data = json.loads(req.content)
