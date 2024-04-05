0473936681cf7c675dbc5724f641802893f85aa3
stephincjose <stephinjosec@gmail.com>
2024-04-05 01:25:57 +0100

    table created

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import json
 
 app = Flask(__name__)
 
- 
- 
 @app.route("/", methods=['GET'])
 def index():
     req = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dublin?unitGroup=metric&include=days%2Chours%2Calerts%2Ccurrent&key=TA8XSWUVK2M3S7GXQHHJ3349L&contentType=json')
@@ -17,7 +15,32 @@ def index():
 
     for hour in data['days'][0]['hours']:
         hourly_data.append({
-            'datetime' : hour['datetime']
+            'datetime' : hour['datetime'],
+            'datetimeEpoch': hour['datetimeEpoch'],
+            'temp': hour['temp'],
+            'feelslike': hour['feelslike'],
+            'humidity' : hour['humidity'],
+            'dew': hour['dew'],
+            'precip': hour['precip'],
+            'precipprob' : hour['precipprob'],
+            'snow' :hour['snow'],
+            'snowdepth' : hour['snowdepth'],
+            'preciptype' : hour['preciptype'],
+            'windgust' : hour['windgust'],
+            'windspeed' : hour['windspeed'],
+            'winddir' : hour['winddir'],
+            'pressure' : hour['pressure'],
+            'visibility' : hour['visibility'],
+            'cloudcover' : hour['cloudcover'],
+            'solarradiation' : hour['solarradiation'],
+            'solarenergy' : hour['solarenergy'],
+            'uvindex' : hour['uvindex'],
+            'severerisk' : hour['severerisk'],
+            'conditions' : hour['conditions'],
+            'icon' : hour['icon'],
+            'stations' : hour['stations'],
+            'source' : hour['source']
+
         })
     
     temperature = data['days'][0]['temp']
