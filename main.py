3e7ad0121c992c5c9cbfbc27f3dd8dfa8494d3e8
stephincjose <stephinjosec@gmail.com>
2024-04-13 22:38:01 +0100

    sorted the daily min max parameter according to the date

--- main.py
+++ main.py
@@ -42,206 +42,182 @@ def index():
     
     #####------------------------------------------->
     
-    for hour in data['days'][0]['hours']:
-            hourly_data.append({
-                'datetime' : hour['datetime'],
-                'datetimeEpoch': hour['datetimeEpoch'],
-                'temp': hour['temp'],
-                'feelslike': hour['feelslike'],
-                'humidity' : hour['humidity'],
-                'dew': hour['dew'],
-                'precip': hour['precip'],
-                'precipprob' : hour['precipprob'],
-                'snow' :hour['snow'],
-                'snowdepth' : hour['snowdepth'],
-                'preciptype' : hour['preciptype'],
-                'windgust' : hour['windgust'],
-                'windspeed' : hour['windspeed'],
-                'winddir' : hour['winddir'],
-                'pressure' : hour['pressure'],
-                'visibility' : hour['visibility'],
-                'cloudcover' : hour['cloudcover'],
-                'solarradiation' : hour['solarradiation'],
-                'solarenergy' : hour['solarenergy'],
-                'uvindex' : hour['uvindex'],
-                'severerisk' : hour['severerisk'],
-                'conditions' : hour['conditions'],
-                'icon' : hour['icon'],
-                'stations' : hour['stations'],
-                'source' : hour['source']
-            })
-            Temperature_h.append(hour['temp'])
-            Feelslike_h.append(hour['feelslike'])
 
-            Humidity_h.append(hour['humidity'])
-            Dew_h.append(hour['dew'])
-            Precip_h.append(hour['precip'])
-            Snow_h.append(hour['snow'])
-            Snowdepth_h.append(hour['snowdepth'])
-            Windgust_h.append(hour['windgust'])
-            Windspeed_h.append(hour['windspeed'])
-            Winddir_h.append(hour['winddir'])
-            Pressure_h.append(hour['pressure'])
-            Visibility_h.append(hour['visibility'])
-            Cloudcover_h.append(hour['cloudcover'])
-            Solarradiation_h.append(hour['solarradiation'])
-            Solarenergy_h.append(hour['solarenergy'])
-            Uvindex_h.append(hour['uvindex'])
-            Severerisk_h.append(hour['severerisk'])
 
-    def findminmaxmean(parameter_name, parameters):
-        parameters_max = np.max(parameters)
-        print(f'Max {parameter_name}:', parameters_max)
-        parameters_min = np.min(parameters)
-        print(f'Min {parameter_name}:', parameters_min)
-        parameters_mean = np.mean(parameters)
-        rounded_parameters_mean = round(parameters_mean, 2)
-        print(f'Mean {parameter_name}:', rounded_parameters_mean)
-        return parameters_max, parameters_min, rounded_parameters_mean
-
-    Temperature_max, Temperature_min, Temperature_mean = findminmaxmean('Temperature_h', Temperature_h)
-    Feelslike_max, Feelslike_min, Feelslike_mean = findminmaxmean('Feelslike', Feelslike_h)
-
-    print(f"Temperature Max: {Temperature_max}, Min: {Temperature_min}, Mean: {Temperature_mean}")
-    print(f"Feelslike Max: {Feelslike_max}, Min: {Feelslike_min}, Mean: {Feelslike_mean}")
-
-    def findmean(parameter_name, parameters):
-        parameters_mean = np.mean(parameters)
-        rounded_parameters_mean = round(parameters_mean, 2)
-        print(f'Mean {parameter_name}:', rounded_parameters_mean)
-        return rounded_parameters_mean
     
-  
-    
-    mean_Humidity=findmean('Humidity', Humidity_h) 
-    mean_Dew_Dew=findmean('Dew', Dew_h)
-    mean_Precip=findmean('Precip', Precip_h)
-    mean_Snow=findmean('Snow', Snow_h)
-    mean_Snowdepth=findmean('Snowdepth', Snowdepth_h)
-    mean_Windgust=findmean('Windgust', Windgust_h)
-    mean_Windspeed=findmean('Windspeed', Windspeed_h)
-    mean_Winddir=findmean('Winddir', Winddir_h)
-    mean_Pressure=findmean('Pressure', Pressure_h)
-    mean_Visibility=findmean('Visibility', Visibility_h)
-    mean_Cloudcover=findmean('Cloudcover', Cloudcover_h)
-    mean_Solarradiation=findmean('Solarradiation', Solarradiation_h)
-    mean_Solarenergy=findmean('Solarenergy', Solarenergy_h)
-    mean_Uvindex=findmean('Uvindex', Uvindex_h)
-    mean_Severerisk=findmean('Severerisk', Severerisk_h)
+    try:
+        # with open('json_monthly_02.txt', 'r') as file:
+        #         data = json.load(file)
+                
+        for hour in data['days'][0]['hours']:
+                hourly_data.append({
+                    'datetime' : hour['datetime'],
+                    'datetimeEpoch': hour['datetimeEpoch'],
+                    'temp': hour['temp'],
+                    'feelslike': hour['feelslike'],
+                    'humidity' : hour['humidity'],
+                    'dew': hour['dew'],
+                    'precip': hour['precip'],
+                    'precipprob' : hour['precipprob'],
+                    'snow' :hour['snow'],
+                    'snowdepth' : hour['snowdepth'],
+                    'preciptype' : hour['preciptype'],
+                    'windgust' : hour['windgust'],
+                    'windspeed' : hour['windspeed'],
+                    'winddir' : hour['winddir'],
+                    'pressure' : hour['pressure'],
+                    'visibility' : hour['visibility'],
+                    'cloudcover' : hour['cloudcover'],
+                    'solarradiation' : hour['solarradiation'],
+                    'solarenergy' : hour['solarenergy'],
+                    'uvindex' : hour['uvindex'],
+                    'severerisk' : hour['severerisk'],
+                    'conditions' : hour['conditions'],
+                    'icon' : hour['icon'],
+                    'stations' : hour['stations'],
+                    'source' : hour['source']
+                })
+                Temperature_h.append(hour['temp'])
+                Feelslike_h.append(hour['feelslike'])
 
-   #####------------------------------------------->
+                Humidity_h.append(hour['humidity'])
+                Dew_h.append(hour['dew'])
+                Precip_h.append(hour['precip'])
+                Snow_h.append(hour['snow'])
+                Snowdepth_h.append(hour['snowdepth'])
+                Windgust_h.append(hour['windgust'])
+                Windspeed_h.append(hour['windspeed'])
+                Winddir_h.append(hour['winddir'])
+                Pressure_h.append(hour['pressure'])
+                Visibility_h.append(hour['visibility'])
+                Cloudcover_h.append(hour['cloudcover'])
+                Solarradiation_h.append(hour['solarradiation'])
+                Solarenergy_h.append(hour['solarenergy'])
+                Uvindex_h.append(hour['uvindex'])
+                Severerisk_h.append(hour['severerisk'])
 
-    datetimeEpoch= data['days'][0]['hours'][0]['datetimeEpoch']
-    timezone_offset = datetime.timedelta(hours=1)  
-    real_time = datetime.datetime.utcfromtimestamp(datetimeEpoch) + timezone_offset
-    date = real_time.strftime('%Y-%m-%d')
-    print("Real Date in GMT+1:", date)
-    
-    daily_parameters=[date, Temperature_max, Temperature_min, Temperature_mean,Feelslike_max, Feelslike_min, Feelslike_mean , mean_Humidity,mean_Dew_Dew,mean_Precip,mean_Snow,mean_Snowdepth,mean_Windgust,mean_Windspeed,mean_Winddir,mean_Pressure,mean_Visibility,mean_Cloudcover,mean_Solarradiation,mean_Solarenergy,mean_Uvindex,mean_Severerisk]
-    daily_parameters_dict={'date':date, 'Temperature_max':Temperature_max, 'Temperature_min':Temperature_min, 'Temperature_mean':Temperature_mean,'Feelslike_max':Feelslike_max, 'Feelslike_min':Feelslike_min, 'Feelslike_mean':Feelslike_mean ,'mean_Humidity': mean_Humidity,'mean_Dew_Dew':mean_Dew_Dew,'mean_Precip':mean_Precip,'mean_Snow':mean_Snow,'mean_Snowdepth':mean_Snowdepth,'mean_Windgust':mean_Windgust,'mean_Windspeed':mean_Windspeed,'mean_Winddir':mean_Winddir,'mean_Pressure':mean_Pressure,'mean_Visibility':mean_Visibility,'mean_Cloudcover':mean_Cloudcover,'mean_Solarradiation':mean_Solarradiation,'mean_Solarenergy':mean_Solarenergy,'mean_Uvindex':mean_Uvindex,'mean_Severerisk':mean_Severerisk}
-    print(len(daily_parameters))
+        def findminmaxmean(parameter_name, parameters):
+            parameters_max = np.max(parameters)
+            print(f'Max {parameter_name}:', parameters_max)
+            parameters_min = np.min(parameters)
+            print(f'Min {parameter_name}:', parameters_min)
+            parameters_mean = np.mean(parameters)
+            rounded_parameters_mean = round(parameters_mean, 2)
+            print(f'Mean {parameter_name}:', rounded_parameters_mean)
+            return parameters_max, parameters_min, rounded_parameters_mean
 
+        Temperature_max, Temperature_min, Temperature_mean = findminmaxmean('Temperature_h', Temperature_h)
+        Feelslike_max, Feelslike_min, Feelslike_mean = findminmaxmean('Feelslike', Feelslike_h)
 
+        print(f"Temperature Max: {Temperature_max}, Min: {Temperature_min}, Mean: {Temperature_mean}")
+        print(f"Feelslike Max: {Feelslike_max}, Min: {Feelslike_min}, Mean: {Feelslike_mean}")
 
+        def findmean(parameter_name, parameters):
+            parameters_mean = np.mean(parameters)
+            rounded_parameters_mean = round(parameters_mean, 2)
+            print(f'Mean {parameter_name}:', rounded_parameters_mean)
+            return rounded_parameters_mean
         
-
-    # try:
-    #     with open('json_monthly_02.txt', 'r') as file:
-    #         data = json.load(file)
-
-    #     x= len(data['days'])    
-    #     #print(x)
-
-    #     for days in range(x-1):
-    #         hourly_data = []
-
-    #         for hour in data['days'][days]['hours']:
-    #             hourly_data.append({
-    #                 'datetime' : hour['datetime'],
-    #                 'datetimeEpoch': hour['datetimeEpoch'],
-    #                 'temp': hour['temp'],
-    #                 'feelslike': hour['feelslike'],
-    #                 'humidity' : hour['humidity'],
-    #                 'dew': hour['dew'],
-    #                 'precip': hour['precip'],
-    #                 'precipprob' : hour['precipprob'],
-    #                 'snow' :hour['snow'],
-    #                 'snowdepth' : hour['snowdepth'],
-    #                 'preciptype' : hour['preciptype'],
-    #                 'windgust' : hour['windgust'],
-    #                 'windspeed' : hour['windspeed'],
-    #                 'winddir' : hour['winddir'],
-    #                 'pressure' : hour['pressure'],
-    #                 'visibility' : hour['visibility'],
-    #                 'cloudcover' : hour['cloudcover'],
-    #                 'solarradiation' : hour['solarradiation'],
-    #                 'solarenergy' : hour['solarenergy'],
-    #                 'uvindex' : hour['uvindex'],
-    #                 'severerisk' : hour['severerisk'],
-    #                 'conditions' : hour['conditions'],
-    #                 'icon' : hour['icon'],
-    #                 'stations' : hour['stations'],
-    #                 'source' : hour['source'] })
-    #         try:    
-    #             for l in range(24):
-    #                 post = hourly_data[l]
-    #                 collection.insert_one(post)
-    #                 print('inserted',post)
-    #         except:
-    #             print('error')
-    # except:
-    #     print('error')  
-
-    #xm = {'datetime': '11:00:00', 'datetimeEpoch': 1713002401, 'temp': 10.1, 'feelslike': 10.1, 'humidity': 71.43, 'dew': 5.1, 'precip': 0.0, 'precipprob': 12.9, 'snow': 0.0, 'snowdepth': 0.0, 'preciptype': None, 'windgust': 51.1, 'windspeed': 24.1, 'winddir': 243.9, 'pressure': 1018.7, 'visibility': 24.0, 'cloudcover': 69.1, 'solarradiation': 307.7, 'solarenergy': 1.1, 'uvindex': 3.0, 'severerisk': 10.0, 'conditions': 'Partially cloudy', 'icon': 'partly-cloudy-day', 'stations': None, 'source': 'fcst'}
-    #collection.insert_one(xm)
-
     
-    for l in range(24):
-                    post = hourly_data[l]
-                    collection.insert_one(post)
-    
-    print('inserted',daily_parameters_dict)
-    filter_date = daily_parameters_dict['date']
-    filter = {'date': filter_date }
+        
+        mean_Humidity=findmean('Humidity', Humidity_h) 
+        mean_Dew_Dew=findmean('Dew', Dew_h)
+        mean_Precip=findmean('Precip', Precip_h)
+        mean_Snow=findmean('Snow', Snow_h)
+        mean_Snowdepth=findmean('Snowdepth', Snowdepth_h)
+        mean_Windgust=findmean('Windgust', Windgust_h)
+        mean_Windspeed=findmean('Windspeed', Windspeed_h)
+        mean_Winddir=findmean('Winddir', Winddir_h)
+        mean_Pressure=findmean('Pressure', Pressure_h)
+        mean_Visibility=findmean('Visibility', Visibility_h)
+        mean_Cloudcover=findmean('Cloudcover', Cloudcover_h)
+        mean_Solarradiation=findmean('Solarradiation', Solarradiation_h)
+        mean_Solarenergy=findmean('Solarenergy', Solarenergy_h)
+        mean_Uvindex=findmean('Uvindex', Uvindex_h)
+        mean_Severerisk=findmean('Severerisk', Severerisk_h)
 
-    doc = collection_min_max.find_one({'date': filter_date })
-    print(filter_date, type(filter_date))
-    
-    if doc:
-        collection_min_max.replace_one(filter,  daily_parameters_dict)
-    else:
-        collection_min_max.insert_one(daily_parameters_dict)
+    #####------------------------------------------->
 
+        datetimeEpoch= data['days'][0]['hours'][0]['datetimeEpoch']
+        timezone_offset = datetime.timedelta(hours=1)  
+        real_time = datetime.datetime.utcfromtimestamp(datetimeEpoch) + timezone_offset
+        date = real_time.strftime('%Y-%m-%d')
+        print("Real Date in GMT+1:", date)
+        
+        daily_parameters=[date, Temperature_max, Temperature_min, Temperature_mean,Feelslike_max, Feelslike_min, Feelslike_mean , mean_Humidity,mean_Dew_Dew,mean_Precip,mean_Snow,mean_Snowdepth,mean_Windgust,mean_Windspeed,mean_Winddir,mean_Pressure,mean_Visibility,mean_Cloudcover,mean_Solarradiation,mean_Solarenergy,mean_Uvindex,mean_Severerisk]
+        # print(len(daily_parameters))
+        daily_parameters_dict={'date':date, 'Temperature_max':Temperature_max, 'Temperature_min':Temperature_min, 'Temperature_mean':Temperature_mean,'Feelslike_max':Feelslike_max, 'Feelslike_min':Feelslike_min, 'Feelslike_mean':Feelslike_mean ,'mean_Humidity': mean_Humidity,'mean_Dew_Dew':mean_Dew_Dew,'mean_Precip':mean_Precip,'mean_Snow':mean_Snow,'mean_Snowdepth':mean_Snowdepth,'mean_Windgust':mean_Windgust,'mean_Windspeed':mean_Windspeed,'mean_Winddir':mean_Winddir,'mean_Pressure':mean_Pressure,'mean_Visibility':mean_Visibility,'mean_Cloudcover':mean_Cloudcover,'mean_Solarradiation':mean_Solarradiation,'mean_Solarenergy':mean_Solarenergy,'mean_Uvindex':mean_Uvindex,'mean_Severerisk':mean_Severerisk}
+    
+
+
+
+            
+
+        # try:
+        #     with open('json_monthly_02.txt', 'r') as file:
+        #         data = json.load(file)
+
+        #     x= len(data['days'])    
+        #     #print(x)
+
+        #     for days in range(x-1):
+        #         hourly_data = []
+
+        #         for hour in data['days'][days]['hours']:
+        #             hourly_data.append({
+        #                 'datetime' : hour['datetime'],
+    
+        
+        for l in range(24):
+                        post = hourly_data[l]
+                        collection.insert_one(post)
+        
+        print('inserted',daily_parameters_dict)
+        filter_date = daily_parameters_dict['date']
+        filter = {'date': filter_date }
+
+
+    # all_data = collection.find().sort("datetimeEpoch", pymongo.DESCENDING)
+#sort("datetimeEpoch", pymongo.DESCENDING)
+
+        doc = collection_min_max.find_one({'date': filter_date })
+        print(filter_date, type(filter_date))
+        
+        if doc:
+            collection_min_max.replace_one(filter,  daily_parameters_dict)
+        else:
+            collection_min_max.insert_one(daily_parameters_dict)
+
+    except:
+        print('error')
                     
     return render_template('index.html', hourly_data= hourly_data, post=post ,data=data , date=date , daily_parameters=daily_parameters)
 
-@app.route('/processed-data-minmax')
-def view_data3():
-    parameter_dicts = []
-    pipeline = [
-        {"$group": {"_id": "$_id", "count": {"$sum": 1}}},
-        {"$match": {"count": {"$gt": 1}}}
-    ] 
-    duplicate_dates = list(collection.aggregate(pipeline))
+# @app.route('/processed-data-minmax')
+# def view_data3():
+#     parameter_dicts = []
+#     pipeline = [
+#         {"$group": {"_id": "$_id", "count": {"$sum": 1}}},
+#         {"$match": {"count": {"$gt": 1}}}
+#     ] 
+#     duplicate_dates = list(collection.aggregate(pipeline))
 
-    for date_doc in duplicate_dates:
-        count = date_doc["count"]
-        date_value = date_doc["_id"]  # Date value
-        date_id = str(date_doc["_id"])  # ObjectId as string
-        print('Count:', count, 'Date:', date_value, 'ObjectId:', date_id)
-
-
-        
-    daily_parameters_list = collection_min_max.find({"date": {"$exists": True}})
+#     for date_doc in duplicate_dates:
+#         count = date_doc["count"]
+#         date_value = date_doc["_id"]  # Date value
+#         date_id = str(date_doc["_id"])  # ObjectId as string
+#         print('Count:', count, 'Date:', date_value, 'ObjectId:', date_id)
+     
+#     daily_parameters_list = collection_min_max.find({"date": {"$exists": True}})
     
     
-    if daily_parameters_list:
-        for daily_parameters_dict in daily_parameters_list:
-            parameter_dicts.append(daily_parameters_dict)
-            print('sfafds',daily_parameters_dict["_id"])
+#     if daily_parameters_list:
+#         for daily_parameters_dict in daily_parameters_list:
+#             parameter_dicts.append(daily_parameters_dict)
+#             print('sfafds',daily_parameters_dict["_id"])
 
-        print('processed out', parameter_dicts)
-        return render_template('processed-data-minmax.html',  parameter_dicts=parameter_dicts)
+#         print('processed out', parameter_dicts)
+#         return render_template('processed-data-minmax.html',  parameter_dicts=parameter_dicts)
 
 
 @app.route('/view-data')
@@ -272,31 +248,19 @@ def view_data1():
     #from viewdata3():
     
     parameter_dicts = []
-    pipeline = [
-        {"$group": {"_id": "$_id", "count": {"$sum": 1}}},
-        {"$match": {"count": {"$gt": 1}}}
-    ] 
-    duplicate_dates = list(collection.aggregate(pipeline))
-
-    for date_doc in duplicate_dates:
-        count = date_doc["count"]
-        date_value = date_doc["_id"]  # Date value
-        date_id = str(date_doc["_id"])  # ObjectId as string
-        print('Count:', count, 'Date:', date_value, 'ObjectId:', date_id)
-
-
         
-    daily_parameters_list = collection_min_max.find({"date": {"$exists": True}})
+    daily_parameters_list = collection_min_max.find().sort("date", pymongo.DESCENDING)
     
     
     if daily_parameters_list:
         for daily_parameters_dict in daily_parameters_list:
             parameter_dicts.append(daily_parameters_dict)
-            print('sfafds',daily_parameters_dict["_id"])
-
-        print('processed out', parameter_dicts)
+        #     print('objectid',daily_parameters_dict["_id"])
+        # print('processed out', parameter_dicts)
 
     ## end viewdata3()
+
+
 
     all_data = collection.find().sort("datetimeEpoch", pymongo.DESCENDING)
     processed_data = []
@@ -362,15 +326,6 @@ def view_data1():
                 x['Clear'] = 'Yes'
             else:
                 x['Clear'] = 'No'  
-
-                      
-
-
-              
-
-         
-         
-
 
 
     for x in processed_data:
